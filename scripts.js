823eeb760d76f726d9e617620d3153fa5e3946c6
Arul <59489321+arulsethi02@users.noreply.github.com>
2024-05-29 00:16:20 -0500

    fix the yawn counter
    was incrementing 0-->2. now should increments 0--> 1

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     var dogImage = document.getElementById("dog-image");
     var clickCounter = document.getElementById("click-counter");
-    var counter = 1;
+    var yawn_count = 0;
     
     dogImage.addEventListener("click", function() {
         if (dogImage.src.includes("dog2.png")) {
             dogImage.src = "dog.png";
-            counter++;
-            clickCounter.textContent = "Yawn Count: " + counter;
+            yawn_count++;
+            clickCounter.textContent = "Yawn Count: " + yawn_count;
         } else {
             dogImage.src = "dog2.png";
         }
