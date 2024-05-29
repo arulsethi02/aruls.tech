3fab176c9db7cf24808c9a36aa241eb46aaf66e9
Arul <59489321+arulsethi02@users.noreply.github.com>
2024-05-29 00:13:16 -0500

    change click counter function to count how many times dog shows up on screen

--- scripts.js
+++ scripts.js
@@ -1,22 +1,15 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     var dogImage = document.getElementById("dog-image");
     var clickCounter = document.getElementById("click-counter");
-    var counter = 0;
-    var clickCount = 0;
+    var counter = 1;
     
     dogImage.addEventListener("click", function() {
-        if (dogImage.src.includes("dog.png")) {
-            dogImage.src = "dog2.png";
-        } else {
+        if (dogImage.src.includes("dog2.png")) {
             dogImage.src = "dog.png";
-        }
-
-        clickCount++;
-        if (clickCount % 2 === 0) {
             counter++;
             clickCounter.textContent = "Click counter: " + counter;
+        } else {
+            dogImage.src = "dog2.png";
         }
     });
 });
-
